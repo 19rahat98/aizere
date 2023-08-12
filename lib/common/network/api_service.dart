090cf58d78b8d90fe19fd137ac85e948b4e1f20e
licantropy <Licantropy96@gmail.com>
2023-08-12 16:17:28 +0600

    fix: fixed library

--- lib/common/network/api_service.dart
+++ lib/common/network/api_service.dart
@@ -1,17 +1,15 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
-
   ApiService(this._httpClient);
 
   final Future<Dio> _httpClient;
 
   Future<Response> getClassLib(param) async {
-
     final client = await _httpClient;
-    return client.get<dynamic>(
+    return client.post<dynamic>(
       '/kazlibrary/get_texts',
-      queryParameters: param,
+      data: param,
     );
   }
 }
